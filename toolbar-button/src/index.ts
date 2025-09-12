d90c219f016d20715718235ce5f9f8bdf7c4db13
Hou-Jen Ko <hou-jen.ko@intel.com>
2025-09-12 14:19:55 -0700

    My working extension

--- toolbar-button/src/index.ts
+++ toolbar-button/src/index.ts
@@ -1,19 +1,101 @@
-import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+import '../style/index.css'
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { INotebookTracker } from '@jupyterlab/notebook';
+import { LabIcon } from '@jupyterlab/ui-components';
+import linkGenSVG from '../link.svg';
+import uploadSVG from '../upload.svg';
 
-/**
- * The plugin registration information.
- */
-const plugin: JupyterFrontEndPlugin<void> = {
+const linkIcon = new LabIcon({
+  name: 'link-icon',
+  svgstr: linkGenSVG
+});
+
+const uploadIcon = new LabIcon({
+  name: 'upload-icon',
+  svgstr: uploadSVG
+});
+
+
+const extension: JupyterFrontEndPlugin<void> = {
   id: '@jupyterlab-examples/toolbar-button:plugin',
   description:
     'A JupyterLab extension adding a button to the Notebook toolbar.',
   autoStart: true,
-  activate: () => {
-    // Nothing is needed
+  requires: [INotebookTracker],
+  activate: (
+    app: JupyterFrontEnd,
+    nbtracker: INotebookTracker
+  ) => {
+    const { commands } = app;
+
+    async function callAPI(data: string) {
+      const url = "https://pisa.intel.com/API/Record";
+      var form = new FormData();
+
+      form.append("Stdin", data);
+      form.append("InputType", ".ipynb");
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', url, true);
+      xhr.withCredentials = true;
+
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          var SHA = xhr.responseText;
+          navigator.clipboard.writeText(
+            "https://wiki.intel.com/Jupyter/lab/?fromURL=https://wiki.intel.com/tmp/" + SHA +
+            ".ipynb"
+          );
+          alert('Copied the link to the clipboard.');
+          console.log(SHA);
+        } else {
+          console.error(xhr.statusText);
+        }
+      };
+      xhr.send(form);
+    }
+
+    async function callNewPR(data: string, filepath: string) {
+      const url = "https://github.com/intel-sandbox/jupyterlite/edit/main/content/" +
+        filepath;
+      navigator.clipboard.writeText(data);
+      alert('Copied the Jupyter notebook to the clipboard.\nPlease paste the content to the opened page as a commit.');
+      const newTab = window.open(url, '_blank');
+      if (newTab) {
+        newTab.focus();
+      } else {
+        console.error("The new tab cannot be opened");
+      }
+    }
+
+    commands.addCommand('jlab-examples/log-messages:logTextMessage', {
+      icon: linkIcon,
+      caption: 'Generate Link for this notebook.',
+      execute: () => {
+        if(nbtracker.currentWidget) {
+          const panel = nbtracker.currentWidget;
+          const content = JSON.stringify(panel.context.model.toJSON());
+          callAPI(content);
+        }
+      }
+    });
+
+    commands.addCommand('jlab-examples/log-messages:uploadTextMessage', {
+      icon: uploadIcon,
+      caption: 'Publish this notebook to Github.',
+      execute: () => {
+        if(nbtracker.currentWidget) {
+          const panel = nbtracker.currentWidget;
+          const filepath = panel.context.path;
+          const content = JSON.stringify(panel.context.model.toJSON());
+          callNewPR(content, filepath);
+        }
+      }
+    });
   }
 };
 
-/**
- * Export the plugin as default.
- */
-export default plugin;
+export default extension;
