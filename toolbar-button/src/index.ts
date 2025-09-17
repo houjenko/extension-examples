d333609924443a0a5a13aca3619af398f54a9079
Hou-Jen Ko <hou-jen.ko@intel.com>
2025-09-16 22:49:24 -0700

    minor fix

--- toolbar-button/src/index.ts
+++ toolbar-button/src/index.ts
@@ -58,17 +58,48 @@ const extension: JupyterFrontEndPlugin<void> = {
       xhr.send(form);
     }
 
-    async function callNewPR(data: string, filepath: string) {
-      const url = "https://github.com/intel-sandbox/jupyterlite/new/main/content/" +
-        filepath;
-      navigator.clipboard.writeText(data);
-      alert('Copied the Jupyter notebook to the clipboard.\nPlease paste the content to the opened page as a commit.');
-      const newTab = window.open(url, '_blank');
-      if (newTab) {
-        newTab.focus();
-      } else {
-        console.error("The new tab cannot be opened");
+    async function checkGitHubFileExists(filePath: string, token: string) {
+      const url = `https://api.github.com/repos/intel-sandbox/jupyterlite/contents/content/${filePath}`;
+
+      try {
+        const response = await fetch(url, { headers: {'Authorization': `token ${token}`} });
+        if (response.status === 200) {
+          // File exists
+          return true;
+        } else if (response.status === 404) {
+          // File not found
+          return false;
+        } else {
+          // Other error (e.g., unauthorized, rate limit exceeded)
+          console.error(`Error checking file existence: ${response.status} - ${response.statusText}`);
+          return false; // Or throw an error
+        }
+      } catch (error) {
+        console.error('Network error or other issue:', error);
+        return false;
       }
+    }
+
+    async function callNewPR(data: string, filepath: string) {
+      require('dotenv').config();
+      const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+      checkGitHubFileExists(filepath, GITHUB_TOKEN)
+        .then(exists => {
+          var url = '';
+          if (exists) {
+            url = "https://github.com/intel-sandbox/jupyterlite/edit/main/content/" + filepath;
+          } else {
+            url = "https://github.com/intel-sandbox/jupyterlite/new/main/content/?filename=" + filepath;
+          }
+          navigator.clipboard.writeText(data);
+          alert('Copied the Jupyter notebook to the clipboard.\nPlease paste the content to the opened page as a commit.');
+          const newTab = window.open(url, '_blank');
+          if (newTab) {
+            newTab.focus();
+          } else {
+            console.error("The new tab cannot be opened");
+          }
+        });
     }
 
     commands.addCommand('jlab-examples/log-messages:logTextMessage', {
